80a2ebb0f619213d5ae544f3fb48a877b1241fb0
Held <plattner.mi@gmail.com>
2026-08-03 09:34:30 +0200

    Delete tools/klima-heizlast/location-core.js

--- tools/klima-heizlast/location-core.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/* Temporäre Kompatibilitätsbrücke für Standortpass V1.5. */
-(function loadSharedLocationCore() {
-  if (window.LocationCore) return;
-  const source = document.currentScript?.src || window.location.href;
-  const url = new URL('../../shared/js/services/location-core.js?v=1', source).href;
-  document.write(`<script src="${url}"><\/script>`);
-})();
